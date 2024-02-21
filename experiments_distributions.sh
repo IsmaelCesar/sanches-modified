698290db26d04f4f0bad6084ee8983e92022963e
Ismael Cesar <leamscesar@gmai.com>
2024-02-21 14:09:16 -0300

    scripts for running Modified Sanchez with the distributions

--- experiments_distributions.sh
+++ experiments_distributions.sh
@@ -11,11 +11,11 @@ do
     do
         for qubit_idx in ${qubits_array[@]}
         do
-            python -m experiment_procedures --results-dir results/runs_shell/lognormal --num-qubits $qubit_idx --state-type lognormal --state-params x_points="(0, 1)" s=1 loc=1 scale=2 --eps $eps_val --run-idx $run_idx --verbose --device CPU
-            python -m experiment_procedures --results-dir results/runs_shell/bimodal --num-qubits $qubit_idx --state-type bimodal --state-params x_points="(0, 1)" loc_bim1=5 scale_bim1=0.5 loc_bim2=15 scale_bim2=2 --eps $eps_val --run-idx $run_idx --verbose --device CPU
+            python -m experiment_procedures --results-dir results/runs_shell/lognormal --num-qubits $qubit_idx  --state-type lognormal  --state-params x_points="(0, 1)" s=1 loc=1 scale=2 --eps $eps_val --run-idx $run_idx --verbose --device CPU
+            python -m experiment_procedures --results-dir results/runs_shell/bimodal --num-qubits $qubit_idx    --state-type bimodal    --state-params x_points="(0, 1)" loc_bim1=5 scale_bim1=0.5 loc_bim2=15 scale_bim2=2 --eps $eps_val --run-idx $run_idx --verbose --device CPU
             python -m experiment_procedures --results-dir results/runs_shell/triangular --num-qubits $qubit_idx --state-type triangular --state-params x_points="(0, 1)" c=1 loc=10 scale=5 --eps $eps_val --run-idx $run_idx --verbose --device CPU
-            python -m experiment_procedures --results-dir results/runs_shell/normal --num-qubits $qubit_idx --state-type normal --state-params x_points="(0, 1)" loc=10 scale=3.5 --eps $eps_val --run-idx $run_idx --verbose --device CPU
-            python -m experiment_procedures --results-dir results/runs_shell/laplace --num-qubits $qubit_idx --state-type laplace --state-params x_points="(0, 1)" loc=10 scale=2 --eps $eps_val --run-idx $run_idx --verbose --device CPU
+            python -m experiment_procedures --results-dir results/runs_shell/normal --num-qubits $qubit_idx     --state-type normal     --state-params x_points="(0, 1)" loc=10 scale=3.5 --eps $eps_val --run-idx $run_idx --verbose --device CPU
+            python -m experiment_procedures --results-dir results/runs_shell/laplace --num-qubits $qubit_idx    --state-type laplace    --state-params x_points="(0, 1)" loc=10 scale=2 --eps $eps_val --run-idx $run_idx --verbose --device CPU
         done
     done
 done
